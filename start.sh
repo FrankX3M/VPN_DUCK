e959faf3d7d2f9493f4175eb6055446908734e60
root <root@vds2730789.my-ihor.ru>
2025-04-25 17:23:01 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -75,39 +75,6 @@ swapon --show
 
 echo "Настройка системы завершена!"
 
-# # Создание docker-compose.yml
-# cat > docker-compose.yml << 'EOL'
-# services:
-#   wireguard:
-#     image: linuxserver/wireguard
-#     container_name: wireguard
-#     cap_add:
-#       - NET_ADMIN
-#       - SYS_MODULE
-#     environment:
-#       - PUID=1000
-#       - PGID=1000
-#       - TZ=Europe/Moscow
-#       - SERVERURL=auto
-#       - SERVERPORT=51820
-#       - PEERS=1
-#       - PEERDNS=auto
-#       - INTERNAL_SUBNET=10.13.13.0
-#     volumes:
-#       - ./config:/config
-#       - /lib/modules:/lib/modules
-#     ports:
-#       - 51820:51820/udp
-#     sysctls:
-#       - net.ipv4.conf.all.src_valid_mark=1
-#     restart: unless-stopped
-# EOL
-
-# # Запуск контейнера
-# docker compose up -d
-
-
-
-# Генерация приватного ключа
-
-# docker compose exec -it wireguard wg genkey
+cd ./wtb
+chmod +x restart.sh
+./restart.sh
